33906be319d09ea6f8fe27bce4e2d89570f47397
Leandro Arturi <lea.arturi@gmail.com>
2025-03-03 16:30:58 -0300

    Implement Factory Method pattern for burger creation and update README

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,81 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from '../helpers/colors.ts'
+
+interface Hamburguer {
+  prepare(): void
+}
+
+class ChickenBurger implements Hamburguer {
+  prepare(): void {
+    console.log('Preparando una %cChickenBurger', COLORS.violet)
+  }
+}
+
+class BeefBurger implements Hamburguer {
+  prepare(): void {
+    console.log('Preparando una %cBeefBurger', COLORS.brown)
+  }
+}
+
+class VeggieBurger implements Hamburguer {
+  prepare(): void {
+    console.log('Preparando una %cVeggieBurger', COLORS.green)
+  }
+}
+
+abstract class HamburguerStore {
+  protected abstract createHamburguer(): Hamburguer
+
+  orderHamburguer(): void {
+    const hamburguer = this.createHamburguer()
+    hamburguer.prepare()
+  }
+}
+
+class ChickenBurgerStore extends HamburguerStore {
+  override createHamburguer(): Hamburguer {
+    return new ChickenBurger()
+  }
+}
+
+class BeefBurgerStore extends HamburguerStore {
+  override createHamburguer(): Hamburguer {
+    return new BeefBurger()
+  }
+}
+
+class VeggieBurgerStore extends HamburguerStore {
+  override createHamburguer(): Hamburguer {
+    return new VeggieBurger()
+  }
+}
+
+function main() {
+  let hamburguerStore: HamburguerStore
+
+  const burguerType = prompt(
+    '¿Qué hamburguesa desea? (1) Pollo (2) Res (3) Veggie'
+  )
+
+  switch (burguerType) {
+    case '1':
+      hamburguerStore = new ChickenBurgerStore()
+      break
+    case '2':
+      hamburguerStore = new BeefBurgerStore()
+      break
+    case '3':
+      hamburguerStore = new VeggieBurgerStore()
+      break
+    default:
+      console.log('Opción no válida')
+      return
+  }
+
+  hamburguerStore.orderHamburguer()
+}
+
+main()
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Patrones de diseño
 
-El código está escrito en TypeScript y usamos Deno para ejecutarlo, puedes usar Bun o Node si lo prefieres.
+El código está escrito en TypeScript y Deno.
 
-Si usas Node, deberás configurar TypeScript, por lo que te recomiendo Deno o Bun.
+```bash
+deno --watch 01-creacionales/01.2-builder.ts
+```
 
-# Recursos adicionales
+## Recursos adicionales
 
 [Fuentes recomendadas](https://gist.github.com/Klerith/f7f558766cb9ad8f36e471cceb5dd910)
