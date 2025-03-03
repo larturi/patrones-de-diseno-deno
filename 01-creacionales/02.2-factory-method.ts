8b9be1a4815c4170c9fa65d26606459675f76f08
Leandro Arturi <lea.arturi@gmail.com>
2025-03-03 16:40:32 -0300

    Refactor Report generation and Factory classes for improved clarity and functionality

--- 01-creacionales/02.2-factory-method.ts
+++ 01-creacionales/02.2-factory-method.ts
@@ -24,34 +24,34 @@
       el prompt para seleccionar el tipo de reporte.
  */
 
-import { COLORS } from '../helpers/colors.ts';
-
 // 1. Definir la interfaz Report
 interface Report {
-  generate(): void;
+  generate(): void
 }
 
 // 2. Clases concretas de Reportes
 // Implementar SalesReport e InventoryReport
 
 class SalesReport implements Report {
-  // TODO: implementar el método e imprimir en consola:
-  // 'Generando reporte de ventas...'
+  generate(): void {
+    console.log('Generando reporte de ventas...')
+  }
 }
 
 class InventoryReport implements Report {
-  // TODO: implementar el método e imprimir en consola:
-  // 'Generando reporte de inventario...'
+  generate(): void {
+    console.log('Generando reporte de inventario...')
+  }
 }
 
 // 3. Clase Base ReportFactory con el Método Factory
 
 abstract class ReportFactory {
-  abstract createReport(): Report;
+  protected abstract createReport(): Report
 
   generateReport(): void {
-    const report = this.createReport();
-    report.generate();
+    const report = this.createReport()
+    report.generate()
   }
 }
 
@@ -59,33 +59,30 @@ abstract class ReportFactory {
 
 class SalesReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error('Method not implemented.');
+    return new SalesReport()
   }
 }
 
 class InventoryReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error('Method not implemented.');
+    return new InventoryReport()
   }
 }
 
 // 5. Código Cliente para Probar
 
 function main() {
-  let reportFactory: ReportFactory;
+  let reportFactory: ReportFactory
 
-  const reportType = prompt(
-    '¿Qué tipo de reporte deseas? %c(sales/inventory)',
-    COLORS.red
-  );
+  const reportType = prompt('¿Qué tipo de reporte deseas? (sales/inventory)')
 
   if (reportType === 'sales') {
-    reportFactory = new SalesReportFactory();
+    reportFactory = new SalesReportFactory()
   } else {
-    reportFactory = new InventoryReportFactory();
+    reportFactory = new InventoryReportFactory()
   }
 
-  reportFactory.generateReport();
+  reportFactory.generateReport()
 }
 
-main();
+main()
