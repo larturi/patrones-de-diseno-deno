78a73f05dae3e5f524d91880e1fcd345972581e0
Leandro Arturi <lea.arturi@gmail.com>
2025-03-06 14:04:16 -0300

    Implement Code Editor state management with undo/redo functionality

--- 01-creacionales/05-inmutabilidad.ts
+++ 01-creacionales/05-inmutabilidad.ts
@@ -6,3 +6,110 @@
  *  * Es útil para mantener un historial de estados en aplicaciones interactivas.
  *
  */
+
+import { COLORS } from '../helpers/colors.ts'
+
+class CodeEditorState {
+  readonly content: string
+  readonly cursorPosition: number
+  readonly unsaveChanges: boolean
+
+  constructor(content: string, cursorPosition: number, unsaveChanges: boolean) {
+    this.content = content
+    this.cursorPosition = cursorPosition
+    this.unsaveChanges = unsaveChanges
+  }
+
+  displayState() {
+    console.log(`Contenido: \n${this.content}`)
+    console.log(`Posición del cursor: ${this.cursorPosition}`)
+    console.log(`Cambios sin guardar: ${this.unsaveChanges}`)
+  }
+
+  copyWith({
+    content,
+    cursorPosition,
+    unsaveChanges
+  }: Partial<CodeEditorState>): CodeEditorState {
+    return new CodeEditorState(
+      content ?? this.content,
+      cursorPosition ?? this.cursorPosition,
+      unsaveChanges ?? this.unsaveChanges
+    )
+  }
+}
+
+class CodeEditorHistory {
+  private history: CodeEditorState[] = []
+  private currentIndex: number = -1
+
+  save(state: CodeEditorState): void {
+    if (this.currentIndex < this.history.length - 1) {
+      this.history = this.history.slice(0, this.currentIndex + 1)
+    }
+
+    this.history.push(state)
+    this.currentIndex++
+  }
+
+  undo(): CodeEditorState | null {
+    if (this.currentIndex > 0) {
+      this.currentIndex--
+      return this.history[this.currentIndex]
+    }
+
+    return null
+  }
+
+  redo(): CodeEditorState | null {
+    if (this.currentIndex < this.history.length - 1) {
+      this.currentIndex++
+      return this.history[this.currentIndex]
+    }
+
+    return null
+  }
+}
+
+function main() {
+  const history = new CodeEditorHistory()
+
+  console.log(`\n%cEstado inicial del editor de código`, COLORS.pink)
+  let editorState = new CodeEditorState(
+    `const message = 'Hola mundo'`,
+    2,
+    false
+  )
+  history.save(editorState)
+  editorState.displayState()
+
+  console.log(`\n%cEstado del editor de código 2`, COLORS.pink)
+  editorState = editorState.copyWith({
+    content: `const message = 'Hola mundo'; \nconsole.log(message);`,
+    cursorPosition: 3,
+    unsaveChanges: true
+  })
+  history.save(editorState)
+  editorState.displayState()
+
+  console.log(`\n%cEstado del editor de código 3`, COLORS.pink)
+  editorState = editorState.copyWith({
+    cursorPosition: 1,
+    unsaveChanges: true
+  })
+  history.save(editorState)
+  editorState.displayState()
+
+  console.log(`\n%cEstado del editor de código despues del undo()`, COLORS.pink)
+  editorState = history.undo()!
+  editorState.displayState()
+
+  console.log(
+    `\n%cEstado del editor de código despues del segundo undo()`,
+    COLORS.pink
+  )
+  editorState = history.undo()!
+  editorState.displayState()
+}
+
+main()
