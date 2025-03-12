02624e1ae1d82265833160af48a1006171c05782
Leandro Arturi <lea.arturi@gmail.com>
2025-03-12 19:12:30 -0300

    Add factory function for multilingual greeting messages

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -1,3 +1,4 @@
+import { COLORS } from '../helpers/colors.ts'
 /**
  * ! Factory Function
  * Es un patrón de diseño que nos permite crear objetos o funciones de manera dinámica que serán
@@ -7,3 +8,32 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+
+type Language = 'es' | 'en' | 'fr' | 'pt'
+
+function createGreeter(lang: Language) {
+  return function (name: string) {
+    const messages = {
+      es: `Hola %c${name}!`,
+      en: `Hello %c${name}!`,
+      fr: `Bonjour %c${name}!`,
+      pt: `Olá %c${name}!`
+    }
+
+    return console.log(messages[lang], COLORS.red)
+  }
+}
+
+function main() {
+  const greeterES = createGreeter('es')
+  const greeterEN = createGreeter('en')
+  const greeterFR = createGreeter('fr')
+  const greeterPT = createGreeter('pt')
+
+  greeterES('Juan')
+  greeterEN('John')
+  greeterFR('Jean')
+  greeterPT('João')
+}
+
+main()
